87467863581ab11067ff8723f9775ee7ae9dc514
HOCHH4U5JUMP3R <116505975+HOCHH4U5JUMP3R@users.noreply.github.com>
2026-09-17 08:20:21 +0200

    Register ticket analytics routes

--- app/start.py
+++ app/start.py
@@ -3,5 +3,6 @@ from . import extra_routes  # noqa: F401
 from . import import_routes  # noqa: F401
 from . import model_routes  # noqa: F401
 from . import modernization_routes  # noqa: F401
+from . import ticket_routes  # noqa: F401
 
 __all__ = ['app']
