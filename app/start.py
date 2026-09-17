b03dac5a9638cc6ae073c3dd1cc7eab34c23c839
HOCHH4U5JUMP3R <116505975+HOCHH4U5JUMP3R@users.noreply.github.com>
2026-09-17 08:01:24 +0200

    Load standalone modernization routes

--- app/start.py
+++ app/start.py
@@ -2,5 +2,6 @@ from .main import app
 from . import extra_routes  # noqa: F401
 from . import import_routes  # noqa: F401
 from . import model_routes  # noqa: F401
+from . import modernization_routes  # noqa: F401
 
 __all__ = ['app']
